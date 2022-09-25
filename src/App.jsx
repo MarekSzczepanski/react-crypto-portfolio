d45cd09d17a39166c9c5c19d2b0b57e4689b4801
Marek <marek@ilieth.at>
2022-09-25 10:00:33 +0200

    Show currencies names proposals while typing

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
-  const all_currencies_names = [];
+  const [allCurrenciesNames, setAllCurrenciesNames] = useState(null);
+  const [currencyNamesProposals, setCurrencyNamesProposals] = useState(null);
 
   useEffect(() => {
     getCurrenciesNames();
   }, [])
 
+  useEffect(() => {
+    renderCurrencyNameProposals();
+  }, [currencyNamesProposals])
+
   const getCurrenciesNames = () => {
     fetch("https://api.coingecko.com/api/v3/coins/list")
     .then((res) => res.json()
     .then((res) => {
-      for (let i=0; i<res.length; i++) {
-        all_currencies_names.push(res[i])
-      }
-      console.log(all_currencies_names)
+      setAllCurrenciesNames(res);
     }))
+  }
+
+  const showCurrencyNameProposals = (e) => {
+    const currencyNameInput = e.target.value;
+    const proposals = allCurrenciesNames.filter(proposal => currencyNameInput.toLowerCase() === proposal.name.substring(0, currencyNameInput.length).toLowerCase());
+    if (!proposals.length) return;
+    setCurrencyNamesProposals(proposals);
+    renderCurrencyNameProposals();
+  }
+
+  const renderCurrencyNameProposals = () => {
+    if (!currencyNamesProposals) return;
+    let divs = [];
+    for (let i=0; i<currencyNamesProposals.length; i++) {
+      if (i>15) break;
+      divs.push(<div key={i}>{currencyNamesProposals[i].name}</div>);
+    }
+    return divs;
   }
 
   return (
     <div className="App">
-      
+      <form>
+        <label htmlFor='currency-name'>Cryptocurrency name</label>
+        <div className='autocomplete'>
+          <input id='currency-name' type='text' onChange={showCurrencyNameProposals}></input>
+          {currencyNamesProposals ? renderCurrencyNameProposals() : null}
+        </div>
+      </form>
     </div>
   );
 }
